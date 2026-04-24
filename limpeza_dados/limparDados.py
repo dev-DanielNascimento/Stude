8b91c97146e8beceb0bed7373bcd2c1165acc039
dev-DanielNascimento <danielcunha12.contato@gmail.com>
2026-04-24 19:16:50 -0300

    feat(limpezaDados): limpeza básica de dados v2

--- limpeza_dados/limparDados.py
+++ limpeza_dados/limparDados.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('data.csv', encoding=utf8-sign)
-df.str.strip()
-print(df.head(10))
+df = pd.read_csv('limpeza_dados/data.csv', encoding='utf8', skipinitialspace=True)
+df = df[]
+df = df.columns.str.strip()
+print(df.head())
